db557e3d1f8c1cb3bd6c7ebdd5c6d8906bea4ad8
diegoalfonsodiaz <diegoalfonsodiaz@gmail.com.com>
2018-11-05 03:22:11 -0600

    crud cliente con bootstraps bbien pro xD

--- pagoplanmovil/urls.py
+++ pagoplanmovil/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     #url(r'^$', views.lista_peliculas, name ='lista_peliculas'),
     #url(r'^pelicula/nueva/$', views.pelicula_nueva, name='pelicula_nueva'),
     url(r'^cliente/new/$', views.cliente_new, name='cliente_new'),
+    url(r'^$', views.cliente_list, name='cliente_list'),
+    url(r'^cliente/(?P<pk>[0-9]+)/$', views.cliente_detail,  name='cliente_detail'),
+    url(r'^cliente/new/$', views.cliente_new, name='cliente_new'),
+    url(r'^cliente/(?P<pk>[0-9]+)/edit/$', views.cliente_edit, name='cliente_edit'),
+    url(r'^cliente/(?P<pk>\d+)/remove/$', views.cliente_remove, name='cliente_remove'),
     ]
--- pagoplanmovil/views.py
+++ pagoplanmovil/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 #librería para manejar el envío de mensajes
-
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .forms import ClienteForm
 from pagoplanmovil.models import Cliente, Mes, Pago, Factura
 from django.shortcuts import redirect
-
-
 
 # Create your views here.
 def cliente_new(request):
@@ -20,4 +18,32 @@ def cliente_new(request):
             return redirect('cliente_detail', pk=post.pk)
     else:
         form = ClienteForm()
+    return render(request, 'pagoplanmovil/cliente_edit.html', {'form': form})
+
+def cliente_list(request):
+    clientes = Cliente.objects.all()
+    return render(request, 'pagoplanmovil/cliente_list.html', {'clientes': clientes})
+
+
+def cliente_detail(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    return render(request, 'pagoplanmovil/cliente_detail.html', {'cliente': cliente})
+
+def cliente_remove(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    cliente.delete()
+    return redirect('cliente_list')
+
+
+def cliente_edit(request, pk):
+    cliente = get_object_or_404(Cliente, pk=pk)
+    if request.method == "POST":
+        form = ClienteForm(request.POST, instance=cliente)
+        if form.is_valid():
+            post = form.save(commit=False)
+
+            post.save()
+            return redirect('cliente_detail', pk=post.pk)
+    else:
+        form = ClienteForm(instance=cliente)
     return render(request, 'pagoplanmovil/cliente_edit.html', {'form': form})
